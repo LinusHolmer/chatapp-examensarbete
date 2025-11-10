2cd7e0cf20132cfa317cca51c9125118463ba2af
LinusHolmer <linus.holmer@stud.sti.se>
2025-11-10 17:20:51 +0100

    ADD: Updates + test proxy.ts
    Updated next.js to version 16.0.1 to use the proxy function.
    Made a simple proxy method that redirects a user to the login page.

---
+++ frontend/nextjs-dashboard/app/login/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./login.module.css"
+
+export default function LoginPage(){
+    return (
+        <main>
+            <h1>Login page</h1>
+            <p className={styles.text}>Welcome</p>
+        </main>
+    );
+}
---
+++ frontend/nextjs-dashboard/proxy.ts
@@ -0,0 +1,10 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export function proxy(request: NextRequest) {
+    console.log("Proxy for:" , request.nextUrl.pathname)
+    return NextResponse.redirect(new URL('/login', request.url))
+}
+
+export const config = {
+    matcher: "/test/:path*",
+}
