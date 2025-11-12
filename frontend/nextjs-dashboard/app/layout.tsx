4fd202791092fc4bb6056a056142fafe0d1647f0
Erik.Knoos <erik.knoos@stud.sti.se>
2025-11-12 14:38:33 +0100

    ADD: Lägg till vänner länk som tar en till en ny sida, css för att göra en nav-bar, css för att göra det snyggare

--- frontend/nextjs-dashboard/app/layout.tsx
+++ frontend/nextjs-dashboard/app/layout.tsx
@@ -1,4 +1,6 @@
-import '../app/ui/global.css';
+import "./ui/global.css";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function RootLayout({
   children,
@@ -7,7 +9,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <nav className="navbar">
+          <Link href="/" className="nav-logo">
+            <Image
+              src="/logo.png"
+              alt="Logo"
+              width={200}
+              height={200}
+              priority
+            />
+          </Link>
+
+          <ul className="nav-links">
+            <li><Link href="/add friends">Lägg till vänner</Link></li>
+            
+          </ul>
+        </nav>
+
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
--- frontend/nextjs-dashboard/app/page.tsx
+++ frontend/nextjs-dashboard/app/page.tsx
@@ -4,12 +4,7 @@ import Image from 'next/image'
 export default function Page() {
   return (
     <main>
-      <Image
-        src="/logo.png"  
-        alt="logo"
-        width={300}                 
-        height={300}
-      />
+      
      
     </main>
   );
